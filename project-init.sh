c4dc1b08ee744afe65ce44d337b6106dd66c9e76
Patrick Wilson <pwilson@patrickwilsonconsulting.com>
2016-01-02 09:59:40 -0800

    made the "master" (vanilla) project assume there is a existing root gradle project

--- project-init.sh
+++ project-init.sh
@@ -3,20 +3,4 @@
 rm README.md
 rm LICENSE
 
-#copy the gradle templates into the config/gradle dir
-mkdir -p config/gradle
-
-ROOT_PROJECT_DIR=`pwd`
-
-cd $ROOT_PROJECT_DIR/config/gradle
-
-curl https://codeload.github.com/patrickianwilson/gradle-templates/zip/master > temp.zip
-unzip temp.zip
-mv gradle-templates-master/* .
-rm -r gradle-templates-master
-rm temp.zip
-
-
-
-cd $ROOT_PROJECT_DIR
 
