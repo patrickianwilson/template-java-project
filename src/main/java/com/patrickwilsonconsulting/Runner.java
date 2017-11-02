36777333f9d5e8b84271454681c08e85022ffae1
Patrick Wilson <patrick.ian.wilson@gmail.com>
2017-11-02 08:06:31 -0700

    Creating a standalone java template that uses the gradle project plugins.

---
+++ src/main/java/com/patrickwilsonconsulting/Runner.java
@@ -0,0 +1,11 @@
+package com.patrickwilsonconsulting;
+
+/**
+ * Created by pwilson on 11/2/17.
+ */
+public class Runner {
+
+    public static void main(String... args ) {
+        System.out.println("Hello World");
+    }
+}
