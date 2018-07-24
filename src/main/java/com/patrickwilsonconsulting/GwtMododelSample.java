a94b37610b3a46f88340d6d1eaf688aa8e599719
Patrick Wilson <patrick.ian.wilson@gmail.com>
2018-07-23 18:42:17 -0700

    Creating a GWT library template
    Quickly create new GWT modules to include in a main GWT application

---
+++ src/main/java/com/patrickwilsonconsulting/GwtMododelSample.java
@@ -0,0 +1,14 @@
+package com.patrickwilsonconsulting;
+
+public class GwtMododelSample {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
