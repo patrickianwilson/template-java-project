375c46da85951148181c22956dc925913a5ef7c1
pwilson <pwilson@inquestdevops.com>
2023-06-02 15:00:48 -0700

    Fixing refresh api script

--- refresh-api.sh
+++ refresh-api.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export WS_URL=http://localhost:8080
-export GROUP_ID=inquestdevops.com
-export SERVICE_ID=%%{{ServiceModuleName.lowerCase}}%%
 export MODULE_PATH=`pwd`
 mkdir -p ${MODULE_PATH}/build/tmp
 
@@ -13,16 +11,15 @@ fi
 java -jar ${MODULE_PATH}/build/tmp/openapi-codegen.jar generate\
  --input-spec ${WS_URL}/openapi.yaml\
  --generator-name java\
- --output ${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID \
+ --output ${MODULE_PATH}/\
  --config ${MODULE_PATH}/config/codegen-java-config.json \
  -p enumClassPrefix=true\
  --skip-validate-spec
 
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/settings.gradle"
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/pom.xml"
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/.travis.yml"
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/build.sbt"
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/gradlew*"
-rm -r "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/gradle/"
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/gradle.properties"
-rm "${MODULE_PATH}/src/$GROUP_ID/$SERVICE_ID/git_push.sh"
+rm "${MODULE_PATH}/settings.gradle"
+rm "${MODULE_PATH}/pom.xml"
+rm "${MODULE_PATH}/.travis.yml"
+rm "${MODULE_PATH}/build.sbt"
+rm -r "${MODULE_PATH}/gradle/"
+rm "${MODULE_PATH}/gradle.properties"
+rm "${MODULE_PATH}/git_push.sh"
