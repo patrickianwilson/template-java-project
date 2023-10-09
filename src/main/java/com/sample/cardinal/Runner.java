38a2d9edb59cb113f9efc4ebb95a5372a65ca8df
pwilson <pwilson@inquestdevops.com>
2023-10-09 08:37:24 -0700

    adding static asset handling to the template (needed to support JS or WASM projects)

--- src/main/java/com/sample/cardinal/Runner.java
+++ src/main/java/com/sample/cardinal/Runner.java
@@ -7,7 +7,12 @@ import com.sample.cardinal.filters.CorsFilter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import javax.servlet.DispatcherType;
@@ -32,11 +37,28 @@ public class Runner {
 
         ServletContextHandler root = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
 
+        root.getMimeTypes().addMimeMapping("wasm", "application/wasm");
+        root.getMimeTypes().addMimeMapping("js", "text/javascript");
+
         root.addEventListener(new ApplicationContextListener(componentName));
 
         root.addFilter(CorsFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
         root.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
 
+        // static resources
+        ResourceHandler staticResourcesHandler = new ResourceHandler();
+        staticResourcesHandler.setResourceBase("assets/");
+        staticResourcesHandler.setMimeTypes(root.getMimeTypes());
+        staticResourcesHandler.setCacheControl("no-store,no-cache,must-revalidate");
+        ContextHandler staticResources = new ContextHandler("/assets");
+        staticResources.getMimeTypes().addMimeMapping("wasm", "application/wasm");
+        staticResources.getMimeTypes().addMimeMapping("js", "text/javascript");
+
+        staticResources.setHandler(staticResourcesHandler);
+
+        HandlerList chainedHandlers = new HandlerList();
+        chainedHandlers.setHandlers(new Handler[]{staticResources, root, new DefaultHandler()});
+        server.setHandler(chainedHandlers);
         try {
             server.start();
         } catch(BindException e) {
