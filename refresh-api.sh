beb5604e2cea9de921a31d19fc2c8720fdc93bb3
Patrick Wilson <patrickianwilson@users.noreply.github.com>
2023-10-20 08:25:08 -0700

    Update refresh-api.sh to update client config to okhttps3

--- refresh-api.sh
+++ refresh-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export WS_URL=http://localhost:8080
+export WS_URL=http://localhost:9080
 export MODULE_PATH=`pwd`
 mkdir -p ${MODULE_PATH}/build/tmp
 
@@ -24,13 +24,15 @@ rm -r "${MODULE_PATH}/gradle/"
 rm "${MODULE_PATH}/gradle.properties"
 rm "${MODULE_PATH}/git_push.sh"
 
-mkdir -p ${MODULE_PATH}/src/main/java/com/inquestdevops/%%{{ServiceModuleName}}%%/service/config
+mkdir -p ${MODULE_PATH}/src/main/java/com/inquestdevops/%%{{ServiceModuleName.lowerCase}}%%/service/config
 
-ClientFactoryContents="package com.inquestdevops.%%{{ServiceModuleName}}%%.service.config;
+ClientFactoryContents="package com.inquestdevops.%%{{ServiceModuleName.lowerCase}}%%.service.config
 
-import com.inquestdevops.%%{{ServiceModuleName}}%%.service.ApiClient;
-import com.inquestdevops.%%{{ServiceModuleName}}%%.service.api.DefaultApi;
+import com.inquestdevops.sandstorm.service.resource.client.ApiClient;
+import com.inquestdevops.sandstorm.service.resource.client.api.DefaultApi;
 import lombok.Getter;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
 
 import java.util.concurrent.TimeUnit;
 
@@ -39,8 +41,8 @@ public class ClientFactory {
     @Getter
     public enum Environments {
         LOCAL(\"http://localhost:8080\"),
-        DEV(\"http://%%{{ServiceModuleName}}%%-dev.inquestdevops.com\"),
-        PROD(\"http://%%{{ServiceModuleName}}%%.inquestdevops.com\");
+        DEV(\"http://%%{{ServiceModuleName.lowerCase}}%%-dev.inquestdevops.com\"),
+        PROD(\"http://%%{{ServiceModuleName.lowerCase}}%%.inquestdevops.com\");
 
         private String baseUrl;
         private int readTimeout = (int) TimeUnit.SECONDS.toMillis(20);
@@ -60,32 +62,47 @@ public class ClientFactory {
 
         );
 
-        client.getApiClient().getHttpClient().setAuthenticator(
-                new RabbitServiceAccountAuthenticator(rabbitBaseUrl, clientId, clientSecret));
+        client.getApiClient().setHttpClient(
+                initHttpClient(
+                        client.getApiClient().getHttpClient().networkInterceptors().get(0),
+                        rabbitBaseUrl,
+                        clientId,
+                        clientSecret));
 
         return client;
 
     }
 
+    private static OkHttpClient initHttpClient(Interceptor progressInterceptor,
+                                        String rabbitBaseUrl,
+                                        String clientId,
+                                        String clientSecret) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addNetworkInterceptor(progressInterceptor);
 
+        builder.authenticator(new RabbitServiceAccountAuthenticator(rabbitBaseUrl, clientId, clientSecret));
 
-
+        return builder.build();
+    }
 }
+
+
 "
-echo "${ClientFactoryContents}" > "${MODULE_PATH}/src/main/java/com/inquestdevops/%%{{ServiceModuleName}}%%/service/config/ClientFactory.java"
+echo "${ClientFactoryContents}" > "${MODULE_PATH}/src/main/java/com/inquestdevops/%%{{ServiceModuleName.lowerCase}}%%/service/config/ClientFactory.java"
 
 
-RabbitServiceAccountAuthenticatorContents="package com.inquestdevops.%%{{ServiceModuleName}}%%.service.config;
+RabbitServiceAccountAuthenticatorContents="package com.inquestdevops.%%{{ServiceModuleName.lowerCase}}%%.service.config;
 
 import com.google.gson.Gson;
-import com.squareup.okhttp.*;
+import okhttp3.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
-import java.net.Proxy;
 
 @Slf4j
 public class RabbitServiceAccountAuthenticator implements Authenticator {
@@ -105,29 +122,11 @@ public class RabbitServiceAccountAuthenticator implements Authenticator {
         this.clientSecret = clientSecret;
     }
 
-    @Override
-    public Request authenticate(Proxy proxy, Response response) throws IOException {
-        log.info(\"Attempting to reauthenticate the client by getting a new Rabbit access token using service account credentials\");
-
-        if (response.priorResponse() != null && response.priorResponse().priorResponse() != null) {
-            //this has been retried twice already, something is wrong.
-            log.error(\"Unable to refresh access token using provided client credentials\");
-            return null;
-        }
-        if (currentToken == null || (response.priorResponse() != null && response.priorResponse().code() == 401)) {
-            this.currentToken = refreshAccessToken(0);
-        }
-
-        return response.request().newBuilder().addHeader(\"Authorization\", \"Bearer \" + currentToken)
-                .build();
-    }
-
     protected String refreshAccessToken(int attemptNum) throws IOException {
         if (attemptNum >= MAX_REFRESH_ATTEMPTS) {
             return null;
         }
         OkHttpClient authClient = new OkHttpClient();
-        authClient.setCache(null);
 
         Response authResp = authClient.newCall(new Request.Builder()
                 .url(String.format(\"%s/token\", this.rabbitServiceBaseUrl))
@@ -156,9 +155,23 @@ public class RabbitServiceAccountAuthenticator implements Authenticator {
         return token.access_token;
     }
 
+
+    @Nullable
     @Override
-    public Request authenticateProxy(Proxy proxy, Response response) throws IOException {
-        return null;
+    public Request authenticate(@Nullable Route route, @NotNull Response response) throws IOException {
+        log.info(\"Attempting to reauthenticate the client by getting a new Rabbit access token using service account credentials\");
+
+        if (response.priorResponse() != null && response.priorResponse().priorResponse() != null) {
+            //this has been retried twice already, something is wrong.
+            log.error(\"Unable to refresh access token using provided client credentials\");
+            return null;
+        }
+        if (currentToken == null || (response.priorResponse() != null && response.priorResponse().code() == 401)) {
+            this.currentToken = refreshAccessToken(0);
+        }
+
+        return response.request().newBuilder().addHeader(\"Authorization\", \"Bearer \" + currentToken)
+                .build();
     }
 
     @Data
@@ -178,5 +191,4 @@ public class RabbitServiceAccountAuthenticator implements Authenticator {
 }
 
 "
-echo "${RabbitServiceAccountAuthenticatorContents}" > "${MODULE_PATH}/src/main/java/com/inquestdevops/%%{{ServiceModuleName}}%%/service/config/RabbitServiceAccountAuthenticator.java"
-
+echo "${RabbitServiceAccountAuthenticatorContents}" > "${MODULE_PATH}/src/main/java/com/inquestdevops/%%{{ServiceModuleName.lowerCase}}%%/service/config/RabbitServiceAccountAuthenticator.java"
